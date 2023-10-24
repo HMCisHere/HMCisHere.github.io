72dd44af9f3bf18ad055c083e6cb0881abf030ee
HMCisHere <8arshia5@gmail.com>
2023-10-24 13:49:42 +0330

    haaa

---
+++ partnerscript.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Work's", "Websie's", "Program's", "Config's", "Design's"],
+        typeSpeed: 115,
+        backSpeed: 75,
+        loop: true
+    });
+
+
+    
+});
