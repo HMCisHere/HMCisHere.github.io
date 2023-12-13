e0f4fd87275fae59450678b02923a360d85402a4
HMCisHere <8arshia5@gmail.com>
2023-12-14 00:09:41 +0330

    cvpart1

---
+++ CV1/partnerscript.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Website", "Program", "Server"],
+        typeSpeed: 115,
+        backSpeed: 75,
+        loop: true
+    });
+
+
+    
+});
---
+++ CV1/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Better", "3D Design", "Responsive"],
+        typeSpeed: 115,
+        backSpeed: 75,
+        loop: true
+    });
+
+
+    
+});
